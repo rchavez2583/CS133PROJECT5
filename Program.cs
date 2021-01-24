402a5ec21ac44881b7d7369617376b001c4fd2ff
rchavez2583 <77551911+rchavez2583@users.noreply.github.com>
2021-01-24 13:51:21 -0700

    Add files via upload
    Add project 5

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS133PROJECT5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Track myTrack = new Track();
+
+            myTrack.AddInstruction("Your History");
+            myTrack.AddInstruction("Click to go back, hold to see hystory");
+            myTrack.AddInstruction("Click to go forward, hold to see hystory");
+            myTrack.AddInstruction("Reload this page");
+
+            myTrack.PrintInstruction();
+
+            myTrack.UndoInstruction();
+
+            myTrack.PrintRedoInstruction();
+
+            myTrack.RedoInstruction();
+
+            myTrack.PrintInstruction();
+        }
+    }
+}
+
+public class Track
+{
+    private Stack<string> instrutionsStack = new Stack<string>();
+
+    private Stack<string> undoInstructionStack = new Stack<string>();
+
+    public void AddInstruction(string newInstruction)
+    {
+        instrutionsStack.Push(newInstruction);
+    }
+
+    public void PrintInstruction()
+    {
+        foreach(var item in instrutionsStack)
+        {
+            Console.WriteLine("The instruction is the following: "
+                              + item);
+        }
+    }
+
+    public void UndoInstruction()
+    {
+       string firstInstruction = instrutionsStack.Pop();
+
+        undoInstructionStack.Push(firstInstruction);
+    }
+
+    public void PrintRedoInstruction()
+    {
+        foreach (var item in undoInstructionStack)
+        {
+            Console.WriteLine("The instruction is the following: "
+                              + item);
+        }
+    }
+
+    public void RedoInstruction()
+    {
+        string firstInstruction = undoInstructionStack.Pop();
+
+        undoInstructionStack.Push(firstInstruction);
+    }
+}
